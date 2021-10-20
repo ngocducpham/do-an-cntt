6f32a31caa2a0bae932571cb09cce9076f6678bb
VoHuuGiau2901 <89368768+VoHuuGiau2901@users.noreply.github.com>
2021-10-20 10:29:09 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,10 +3,9 @@ import React from 'react';
 document.getElementById('');
 alert('Hello');
 
+document.getElementById('cc').innerText = "cc";
+
 console.log('Cho em kèo trên');
-<<<<<<< HEAD
 
 console.log('10km')
-=======
 console.log('Hien ne');
->>>>>>> fe7ac236d68b3598456a711fff96fe94ba374ec7
